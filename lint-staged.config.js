852629560e1802e0f02b87d3ff627cd3f21052cd
Karel Zamazal <kai@Mac.lan>
2025-05-19 17:20:09 +0700

    wip

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -10,7 +10,11 @@
  * @type {import('lint-staged').Configuration}
  */
 module.exports = {
-  '**/*.{tsx,ts,js}': ['prettier --write --end-of-line lf', 'eslint --fix'],
+  '**/*.{tsx,ts,js}': [
+    'prettier --write --end-of-line lf',
+    'eslint --fix',
+    'eslint',
+  ],
   '**/*.{json,md,yml}': ['prettier --write --end-of-line lf'],
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 };
