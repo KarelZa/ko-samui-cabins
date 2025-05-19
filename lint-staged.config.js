47008b2f9b2e86b2870e47475f4e47a0bb085ebe
Karel Zamazal <kai@Mac.lan>
2025-05-19 17:47:12 +0700

    husky setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,20 +1,8 @@
-/* eslint-env node */
-// const path = require('path');
-// const formatCommand = 'prettier . --check';
-
-// module.exports = {
-//   '*': formatCommand,
-// };
-
 /**
  * @type {import('lint-staged').Configuration}
  */
 module.exports = {
-  '**/*.{tsx,ts,js}': [
-    'prettier --write --end-of-line lf',
-    'eslint --fix',
-    'eslint --max-warnings=0',
-  ],
+  '**/*.{tsx,ts,js}': ['prettier --write --end-of-line lf', 'eslint --fix'],
   '**/*.{json,md,yml}': ['prettier --write --end-of-line lf'],
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 };
--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,4 +1,3 @@
-import { UserList } from '@/src/components/UserList';
 import React from 'react';
 
 export const metadata = {
@@ -6,12 +5,10 @@ export const metadata = {
 };
 
 const Page = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-
   return (
     <div>
       <h2>CABINS</h2>
+      <h1>test</h1>
     </div>
   );
 };
